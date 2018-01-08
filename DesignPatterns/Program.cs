525509af827d23ce87fdc26b91aac94dcda07c8c
Alexander Chetcuti <chetcuti.alex@gmail.com>
2018-01-08 16:04:49 +0100

    Solid Design Patterns

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DesignPatterns.SolidDesignPrinciples;
+
+namespace DesignPatterns
+{
+    public class Program
+	{
+		static void Main(string[] args)
+		{
+			SingleResponsibilityPrinciple.run();
+			OpenClosedPrinciple.run();
+
+			if (System.Diagnostics.Debugger.IsAttached) Console.ReadKey();
+		}
+    }
+}
