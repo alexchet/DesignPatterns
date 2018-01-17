fd1852bdc1bf9e3f31ab63a21876ec7208080e93
Alexander Chetcuti <chetcuti.alex@gmail.com>
2018-01-17 16:55:25 +0100

    Inner Factory

--- DesignPatterns/FactoryPattern/Factory.cs
+++ DesignPatterns/FactoryPattern/Factory.cs
@@ -4,23 +4,11 @@ using System.Text;
 
 namespace DesignPatterns.FactoryPattern
 {
-	public static class PointFactory
-	{
-		public static Point NewCartesianPoint(double x, double y)
-		{
-			return new Point(x, y);
-		}
-		public static Point NewPolarPoint(double rho, double theta)
-		{
-			return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-		}
-	}
-
 	public class Point
 	{
 		private double x, y;
 
-		public Point(double x, double y)
+		private Point(double x, double y)
 		{
 			this.x = x;
 			this.y = y;
@@ -30,13 +18,25 @@ namespace DesignPatterns.FactoryPattern
 		{
 			return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
 		}
+		
+		public static class Factory
+		{
+			public static Point NewCartesianPoint(double x, double y)
+			{
+				return new Point(x, y);
+			}
+			public static Point NewPolarPoint(double rho, double theta)
+			{
+				return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+			}
+		}
 	}
 
     public static class Factory
     {
 		public static void run()
 		{
-			var point = PointFactory.NewPolarPoint(1.0, Math.PI / 2);
+			var point = Point.Factory.NewPolarPoint(1.0, Math.PI / 2);
 			Console.WriteLine(point);
 		}
     }
