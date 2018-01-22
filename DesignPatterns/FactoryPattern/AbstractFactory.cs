09c02ed54659b2b3c4edf99c549cfd3d67948665
Alexander Chetcuti <chetcuti.alex@gmail.com>
2018-01-22 17:00:15 +0100

    Abstract factory, while considering the open close principle

--- DesignPatterns/FactoryPattern/AbstractFactory.cs
+++ DesignPatterns/FactoryPattern/AbstractFactory.cs
@@ -39,12 +39,79 @@ namespace DesignPatterns.FactoryPattern
 		}
 	}
 
-	internal class TeaFactory : IHotDrinkFactory
+	internal class CoffeeFactory : IHotDrinkFactory
 	{
 		public IHotDrink Prepare(int amount)
 		{
-			Console.WriteLine($"Put in a tea bag, boil water, pour {amount} ml.");
-			return new Tea();
+			Console.WriteLine($"Grind some beans, boil water, pour {amount} ml.");
+			return new Coffee();
+		}
+	}
+
+	public class HotDrinkMachine
+	{
+		//Commented code breaks Open/Close Priniciple
+		//public enum AvailableDrink
+		//{
+		//	Coffee, Tea
+		//}
+
+		//private Dictionary<AvailableDrink, IHotDrinkFactory> factories = new Dictionary<AvailableDrink, IHotDrinkFactory>();
+
+		//public HotDrinkMachine()
+		//{
+		//	foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+		//	{
+		//		var factory = (IHotDrinkFactory)Activator.CreateInstance(
+		//			Type.GetType("DesignPatterns.FactoryPattern." + Enum.GetName(typeof(AvailableDrink), drink) + "Factory"));
+
+		//		factories.Add(drink, factory);
+		//	}
+		//}
+
+		//public IHotDrink MakeDrink(AvailableDrink drink, int amount)
+		//{
+		//	return factories[drink].Prepare(amount);
+		//}
+		private List<Tuple<string, IHotDrinkFactory>> factories = new List<Tuple<string, IHotDrinkFactory>>();
+
+		public HotDrinkMachine()
+		{
+			foreach (var t in typeof(HotDrinkMachine).Assembly.GetTypes())
+			{
+				if (typeof(IHotDrinkFactory).IsAssignableFrom(t) && !t.IsInterface)
+				{
+					factories.Add(Tuple.Create(
+						t.Name.Replace("Factory", string.Empty), (IHotDrinkFactory)Activator.CreateInstance(t)
+						));
+				}
+			}
+		}
+
+		public IHotDrink MakeDrink()
+		{
+			Console.WriteLine("Available Drinks: ");
+			for (var i = 0; i < factories.Count; i++)
+			{
+				var tuple = factories[i];
+				Console.WriteLine($"{i}: {tuple.Item1}");
+			}
+
+			while (true)
+			{
+				string s;
+				if ((s = Console.ReadLine()) != null && int.TryParse(s, out int i) && i >= 0 && i < factories.Count)
+				{
+					Console.Write("Specify amount: ");
+					s = Console.ReadLine();
+					if (s != null && int.TryParse(s, out int amount) && amount > 0)
+					{
+						return factories[i].Item2.Prepare(amount);
+					}
+				}
+
+				Console.WriteLine("Incorrect input, please try again");
+			}
 		}
 	}
 
@@ -52,6 +119,14 @@ namespace DesignPatterns.FactoryPattern
     {
 		public static void run()
 		{
+			//Commented code breaks Open/Close Priniciple
+			//var machine = new HotDrinkMachine();
+			//var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 100);
+			//drink.Consume();
+
+			var machine = new HotDrinkMachine();
+			var drink = machine.MakeDrink();
+			drink.Consume();
 		}
-    }
+	}
 }
